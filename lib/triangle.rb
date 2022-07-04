00a5f802fdfa7b2a3e521d67b15a2f74bfd6240a
Ryan DeLong <rdelong111@gmail.com>
2022-07-04 14:11:11 -0500

    commit tree 639098307509ca30f6c0ca8a191441bf738bc1a4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_reader :sides
+
+  def initialize(s1, s2 ,s3)
+    @sides = [s1, s2, s3, s1, s2]
+  end
+
+  def kind
+    matches = 0
+    3.times do |i|
+      if @sides[i] <= 0 || @sides[i] + @sides[i + 1] <= @sides[i + 2]
+        raise TriangleError
+      elsif @sides[i] == @sides[i + 1]
+        matches += 1
+      end
+    end
+
+    if matches == 3
+      :equilateral
+    elsif matches == 1
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a valid triangle."
+    end
+  end
 end
